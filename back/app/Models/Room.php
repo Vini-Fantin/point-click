59aecde8dd420e5877056c56a3e2c1c2633ca584
Vini-Fantin <vini.fantin.vini@gmail.com>
2025-10-17 09:33:24 -0300

    Add models no projeto

--- back/app/Models/Room.php
+++ back/app/Models/Room.php
@@ -3,19 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Item;
+use App\Models\Place;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Place extends Model
+class Room extends Model
 {
 
     protected $fillable = [
         'name', 
-        'room_id'
     ];
 
-    public function items() {
-        return $this->hasMany(Item::class);
+    public function places() {
+        return $this->hasMany(Place::class);
     }
 
     public $timestamps = false;
